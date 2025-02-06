7dbf88727bb565e0f994f05e5a197a0ed188d2d7
ugniususc <ugniususc@gmail.com>
2025-02-06 11:24:13 +0200

    product list finished for now

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -2,6 +2,7 @@ import { wixClientServer } from "@/lib/wixClientServer";
 import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
+import DOMPurify from "isomorphic-dompurify";
 
 const PRODUCT_PER_PAGE = 8;
 
@@ -52,11 +53,16 @@ const ProductList = async ({
             <span className="font-semibold">${product.price?.price}</span>
           </div>
           {product.additionalInfoSections && (
-            <div className="text-sm text-gray-500">
-              {product.additionalInfoSections.find(
-                (section: any) => section.title === "shortDesc"
-              )?.description || ""}
-            </div>
+            <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  product.additionalInfoSections.find(
+                    (section: any) => section.title === "shortDesc"
+                  )?.description || ""
+                ),
+              }}
+            ></div>
           )}
           <button className="rounded-2xl ring-1 ring-primary text-primary w-max py-2 px-4 text-xs hover:bg-primary hover:text-white">
             Add to Cart
